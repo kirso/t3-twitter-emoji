5a98107e8dfc93f7f7b90eb18e1c2965a4c269cb
Kirill So <kirill.sofronov@gmail.com>
2023-03-25 13:54:35 +0800

    Add Posts Router and Post model to Prisma schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
